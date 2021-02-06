62b07cc202c3c9059e5fef6956972f81eac284cc
Alexandre Carvalho <alexandre.carvalho@live.com>
2021-02-06 15:38:14 -0300

    Implementação parcial

--- frontend/jsl-cad-caminhoneiro-web/src/app/app-routing.module.ts
+++ frontend/jsl-cad-caminhoneiro-web/src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MarcaCaminhaoComponent } from './marca-caminhao/marca-caminhao.component';
 import { MarcaCaminhaoEditarComponent } from './marca-caminhao/marca-caminhao-editar-component/marca-caminhao-editar.component';
+import { ModeloCaminhaoComponent } from './modelo-caminhao/modelo-caminhao.component';
+import { ModeloCaminhaoEditarComponent } from './modelo-caminhao/modelo-caminhao-editar/modelo-caminhao-editar.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'marcasCaminhao', component: MarcaCaminhaoComponent },
   { path: 'marcaCaminhao/:id', component: MarcaCaminhaoEditarComponent },
   { path: 'marcaCaminhao', component: MarcaCaminhaoEditarComponent },
+  { path: 'modelosCaminhao', component: ModeloCaminhaoComponent },
+  { path: 'modeloCaminhao/:id', component: ModeloCaminhaoEditarComponent },
+  { path: 'modeloCaminhao', component: ModeloCaminhaoEditarComponent },
 ];
 
 @NgModule({
